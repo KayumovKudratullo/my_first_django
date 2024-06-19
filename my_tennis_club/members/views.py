d5fb25e71b4c08aee8454d0c278f86dce084175f
qayumovvvvvv <qayumovqudratullo17@gmail.com>
2024-06-19 16:47:45 +0500

    update 19.06

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member
+from django.db.models import Q
 
 def members(request):
     mymembers = Member.objects.all().values()
@@ -22,8 +23,30 @@ def main(request):
     template = loader.get_template('main.html')
     return HttpResponse(template.render())
 
+# def testing(request):
+#     mymembers = Member.objects.all().values()
+#     template = loader.get_template('template.html')
+#     context = {
+#         'mymembers': mymembers
+#     }
+#     return HttpResponse(template.render(context, request))
+
 def testing(request):
-    mymembers = Member.objects.all().values()
+    # 1.
+    # mymembers = Member.objects.filter(firstname='Kudratullo').values()
+    
+    # 2.
+    # mymembers = Member.objects.filter( id = 7, firstname = "Jane").values()
+
+    # 3.  way 1
+    # mymembers = Member.objects.filter( id = 7).values() | Member.objects.filter(id = 8).values()
+           
+        #    way 2
+    # mymembers = Member.objects.filter(Q(id = 4) | Q(id = 7))
+
+    # 4
+    mymembers = Member.objects.filter(firstname__icontains = 'l').values()
+
     template = loader.get_template('template.html')
     context = {
         'mymembers': mymembers
